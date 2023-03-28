4b8e08588bbec11c5ca066b37ea40874c4acafab
gowaae <gowaa@bk.ru>
2023-03-28 08:58:53 +0300

    Add task

---
+++ hw4/Main.java
@@ -0,0 +1,29 @@
+package hw4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * Запускать через Debug с точкой останова на 20-й строке.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        final RedBlackTree tree = new RedBlackTree();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            while (true) {
+                try {
+                    int value = Integer.parseInt(reader.readLine());
+                    tree.add(value);
+                    System.out.println("finish");
+                } catch (Exception ignored) {
+
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
